3ac657f9cfa4857da458fd62950d046d6b042729
Atisam <atisam82@gmail.com>
2026-02-27 21:09:58 +0500

    update

--- classes/form/description.php
+++ classes/form/description.php
@@ -9,6 +9,24 @@ use moodleform;
 class description extends moodleform {
     public function definition() {
         $mform = $this->_form;
-        $mform->addElement('html', '<p>This is the description form</p>');
+        $mform->addElement('html', '<div class="form_section">');
+        $mform->addElement('html', '<h3 class="heading mb-1">'.'Group Description'.'</h3>');
+        $mform->addElement('html', '<h3 class="prime_heading font-400 mb-3 mt-0">'.'This Group description that will go on your public profile. '.'</h3>');
+        // 
+        $mform->addElement('textarea', 'experience', '', [
+            'rows' => 5,
+            'class' => 'lat-textarea',
+            'placeholder' => '',
+        ]);
+
+        $mform->addElement('html', '<h3 class="heading_secondary mb-3 mt-0">'.'Specialities'.'</h3>');
+        $mform->addElement('html', '<div class="checkboxes_section">');
+        $mform->addElement('advcheckbox', 'language_1', 'Conversational English');
+        $mform->addElement('advcheckbox', 'language_1', 'Business English');
+        $mform->addElement('advcheckbox', 'language_1', 'English for beginners');
+        $mform->addElement('html', '</div">');
+        $mform->addElement('html', '</div">');
+
+
     }
 }
